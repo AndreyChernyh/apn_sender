642448fc80010480787932d1d52d1a9cc2e69088
Andrey Chernih <andrey.chernih@gmail.com>
2012-06-27 13:38:18 +0400

    ability to save PID in a file

--- lib/apn/tasks.rb
+++ lib/apn/tasks.rb
@@ -8,6 +8,10 @@ namespace :apn do
   task :sender => :setup do
     require 'apn'
 
+    if ENV['PIDFILE']
+      File.open(ENV['PIDFILE'], 'w') { |f| f << Process.pid }
+    end
+
     worker = APN::Sender.new(:full_cert_path => ENV['FULL_CERT_PATH'], :cert_path => ENV['CERT_PATH'], :environment => ENV['ENVIRONMENT'], :cert_pass => ENV['CERT_PASS'])
     worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
     worker.very_verbose = ENV['VVERBOSE']
